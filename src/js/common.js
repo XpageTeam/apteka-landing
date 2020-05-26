b0ea0c721d4cac45b2ea02c0fcdf9cbc577c15c4
Sergey Gornostaev <sergey1008@gmail.com>
2020-05-26 11:34:04 +0500

    сделал десктоп

--- src/js/common.js
+++ src/js/common.js
@@ -17,3 +17,85 @@ document.addEventListener("DOMContentLoaded", function(){
 		transitionEffect: "slide",
 	});
 });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	
+
+	const mainSlider = document.querySelector('.main-slider .swiper-list');
+	
+	if (!mainSlider)
+		return
+
+
+	import("swiper/js/swiper.esm.js").then(function(Module){	
+
+		const {Swiper, Navigation, EffectFade, Pagination, Lazy, Autoplay} = Module;
+
+		Swiper.use([Navigation, EffectFade, Pagination, Lazy, Autoplay]);
+
+		
+		var slider = new Swiper(mainSlider, {
+			effect: "fade",
+			preloadImages: false,
+			autoplay: true,
+		    lazy: {
+				loadPrevNext: true,
+			},
+			fadeEffect: {
+			    crossFade: true
+			},
+			loop: false,
+			navigation: {
+		        nextEl: '.main-slider .swiper-button-next',
+		        prevEl: '.main-slider .swiper-button-prev',
+		    },
+		    pagination: {
+		        el: '.main-slider .swiper-pagination',
+		        type: 'bullets',
+		        clickable: true,
+		    },
+		});
+
+	})
+})
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	
+
+	const gallerySlider = document.querySelector('.gallery .swiper-list');
+	
+	if (!gallerySlider)
+		return
+
+
+	import("swiper/js/swiper.esm.js").then(function(Module){	
+
+		const {Swiper, Navigation, Pagination, Lazy, Autoplay} = Module;
+
+		Swiper.use([Navigation, Pagination, Lazy, Autoplay]);
+
+		
+		var slider = new Swiper(gallerySlider, {
+			preloadImages: false,
+			slidesPerView: 2,
+			spaceBetween: 100,
+			autoplay: true,
+		    lazy: {
+				loadPrevNext: true,
+			},
+			loop: false,
+			navigation: {
+		        nextEl: '.gallery .swiper-button-next',
+		        prevEl: '.gallery .swiper-button-prev',
+		    },
+		    pagination: {
+		        el: '.gallery .swiper-pagination',
+		        type: 'bullets',
+		        clickable: true,
+		    },
+		});
+
+	})
+})
---
+++ src/ts/common.ts
@@ -0,0 +1,3 @@
+import { domReady, App, sameHeights, EventListener } from "./xpage/index";
+
+import "./tabs";
